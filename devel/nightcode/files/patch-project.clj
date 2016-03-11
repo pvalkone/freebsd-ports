13dd5939ea8b6d10f80034049320b633133baa43
Petteri Valkonen <petteri.valkonen@iki.fi>
2016-03-12 00:07:32 +0200

    Add devel/nightcode

---
+++ devel/nightcode/files/patch-project.clj
@@ -0,0 +1,10 @@
+--- project.clj.orig	2016-01-30 13:54:28 UTC
++++ project.clj
+@@ -3,6 +3,7 @@
+   :url "https://github.com/oakes/Nightcode"
+   :license {:name "Public Domain"
+             :url "http://unlicense.org/UNLICENSE"}
++  :local-repo ".m2"
+   :dependencies [[com.fifesoft/autocomplete "2.5.8"]
+                  [com.fifesoft/rsyntaxtextarea "2.5.8"]
+                  [com.github.insubstantial/substance "7.3"]
